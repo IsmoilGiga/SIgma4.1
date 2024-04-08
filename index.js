979916a51604e0824135a7cd179510163d009695
IsmoilGiga <ismoilagzamovman@gmail.com>
2024-04-08 12:05:20 +0500

    Sigma4.1

---
+++ index.js
@@ -0,0 +1,7 @@
+let ism = prompt("Ismingiz nima: "); 
+let familiya = prompt("Familiyangiz nima: ");
+let yosh = prompt("Yoshingiz nechada: ");
+let fan = prompt("Yoqtirgan faningizni nima: ");
+let tuman = prompt("Yashaydigan tumaningizni nima: ");
+alert(ism + " " + familiya + " " + yosh + " yosh - " + fan + " fan -" + tuman + " tumani" + "!");
+console.log(ism + " " + familiya + " " + yosh + " yosh - " + fan + " fan - "  + tuman + " tumani" + "!")
